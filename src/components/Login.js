3dcff687b738a8c126d8c2815fc6ee3f3d5fb426
Jorge <martaprietoluque@gmail.com>
2019-09-30 13:05:44 +0200

    Add input on Login component

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Login = props => {
+
+
+    return (
+      <main>
+          <label htmlFor="email">Introduce tu email</label>
+         <input type="text" id="email" name="email"/>
+      </main>
+    );
+  };
+  
+  export default Login;
