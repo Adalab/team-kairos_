04a4a986ce5b10671d36c60b1fd4b81457dad672
Jorge <martaprietoluque@gmail.com>
2019-09-30 13:25:59 +0200

    Add initial scss on Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import '../styles/Login.scss';
 
 
 const Login = props => {
 
 
     return (
-        <main>
+        <main className="main__login">
+            <div className="login__email-container">
             <label htmlFor="email">Introduce tu email</label>
-            <input type="text" id="email" name="email" />
-            <label for="rol">Rol</label>
-            <select id="rol" name="rol">
-                <option value="head">head</option>
-                <option value="operaciones">operaciones</option>
-                <option value="talento">talento</option>
-                <option value="embajada">embajada</option>
-            </select>
+            <input className="email" type="text" id="email" name="email"/>
+            </div>
+            <div className="login__rol-container">
+                <label for="rol">Rol</label>
+                <select id="rol" name="rol">
+                    <option value="head">head</option>
+                    <option value="operaciones">operaciones</option>
+                    <option value="talento">talento</option>
+                    <option value="embajada">embajada</option>
+                </select>
+            </div>
         </main>
     );
 };
