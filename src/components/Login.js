6ff63ce670669f3e2067979c8d0bafee5d38e396
Jorge <martaprietoluque@gmail.com>
2019-09-30 13:10:37 +0200

    Add select for rol and input for mail

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,11 +5,18 @@ const Login = props => {
 
 
     return (
-      <main>
-          <label htmlFor="email">Introduce tu email</label>
-         <input type="text" id="email" name="email"/>
-      </main>
+        <main>
+            <label htmlFor="email">Introduce tu email</label>
+            <input type="text" id="email" name="email" />
+            <label for="rol">Rol</label>
+            <select id="rol" name="rol">
+                <option value="head">head</option>
+                <option value="operaciones">operaciones</option>
+                <option value="talento">talento</option>
+                <option value="embajada">embajada</option>
+            </select>
+        </main>
     );
-  };
-  
-  export default Login;
+};
+
+export default Login;
