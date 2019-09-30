d5448fe9f61e6d8d81a37855f72f2e2361f90ef6
silvia <silviademiguel7@gmail.com>
2019-09-30 16:00:48 +0200

    link data user to state

--- src/App.js
+++ src/App.js
@@ -4,27 +4,36 @@ import Header from './components/Header';
 import './App.scss';
 import Login from './components/Login';
 import DevelopersList from './components/DevelopersList';
-import  {users,devAsignation} from './components/Data';
+import { devAsignation } from './components/Data';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      users: users,
+    this.state = {
+      user: { email: '', rol: '' },
       devAsignation: devAsignation,
     };
+    this.getUserData = this.getUserData.bind(this);
+  }
+  getUserData(event) {
+    const value = event.currentTarget.value;
+    const id = event.currentTarget.id;
+    this.setState({ [id]: value });
+    console.log(value);
   }
 
   render() {
-    const {devAsignation}= this.state;
+    const { users, devAsignation } = this.state;
     return (
       <div className="app">
         <Header />
-        <Login />
-        <DevelopersList devAsignation={devAsignation}/>
-       
-         {/* <Form /> */}
+        <Login
+          users={users}
+          getUserData={this.getUserData} />
+        <DevelopersList devAsignation={devAsignation} />
+
+        {/* <Form /> */}
       </div>
     );
   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../styles/Login.scss';
 
 
 const Login = props => {
 
-
-    return (
-        <main className="main__login">
-            <div className="login__email-container">
-            <label htmlFor="email">Introduce tu email</label>
-            <input className="email" type="text" id="email" name="email"/>
-            </div>
-            <div className="login__rol-container">
-                <label htmlFor="rol">Rol</label>
-                <select id="rol" name="rol">
-                    <option value="head">head</option>
-                    <option value="operaciones">operaciones</option>
-                    <option value="talento">talento</option>
-                    <option value="embajada">embajada</option>
-                </select>
-            </div>
-        </main>
-    );
+  const { user, getUserData } = props;
+  return (
+    <main className="main__login">
+      <div className="login__email-container">
+        <label htmlFor="email">Introduce tu email</label>
+        <input className="email" type="text" id="email" name="email" onChange={getUserData} value={user} />
+      </div>
+      <div className="login__rol-container">
+        <label htmlFor="rol">Rol</label>
+        <select id="rol" name="rol" onChange={getUserData}>
+          <option value="">elige tu rol</option>
+          <option value="head">head</option>
+          <option value="operaciones">operaciones</option>
+          <option value="talento">talento</option>
+          <option value="embajada">embajada</option>
+        </select>
+      </div>
+    </main>
+  );
 };
+
+Login.propTypes = {
+  user: PropTypes.arrayOf(PropTypes.object).isRequired,
+  getUserData: PropTypes.func.isRequired
+}
 
 export default Login;
