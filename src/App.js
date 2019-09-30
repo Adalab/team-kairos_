2d292993faddfd3ed7e97232d78bc44386178121
silvia <silviademiguel7@gmail.com>
2019-09-30 15:21:15 +0200

    add list developers

--- src/App.js
+++ src/App.js
@@ -3,19 +3,40 @@ import Header from './components/Header';
 import Form from './components/Form';
 import './App.scss';
 import Login from './components/Login';
+import  {users,devAsignation} from './components/Data';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state={
+      users: users,
+      devAsignation: devAsignation,
+    };
   }
 
   render() {
+    const {devAsignation}= this.state;
     return (
       <div className="app">
         <Header />
-        <Form />
         <Login />
+        <main className="main__developers">
+          <ul className="developers__list">
+          {devAsignation.map( dev => {
+            return(
+              <li>
+                <div className="developer__card-container">
+                  <h3 className="developer__name">{dev.developer}</h3>
+                  <p className="developer__code">{dev.code}</p>
+                  <p className="developer__status">fase: {dev.status}</p>
+                </div>
+              </li>
+            );
+          })}
+          </ul>
+        </main>
+         {/* <Form /> */}
       </div>
     );
   }
