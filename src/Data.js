3fa43af7aa6033641c98575df156034c05eb67f1
claradios <clara.dios.diez@gmail.com>
2019-09-30 12:33:30 +0200

    initial commit

---
+++ src/Data.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+const users = [
+    {
+        email: 'head@kairos.com',
+        rol: 'head'
+    },
+    {
+        email: 'operaciones@kairos.com',
+        rol: 'operaciones'
+    },
+    {
+        email: 'talento@kairos.com',
+        rol: 'talento'
+    },
+    {
+        email: 'embajada@kairos.com',
+        rol: 'embajada'
+    }
+];
+
+const item = [
+    {
+        developer: 'Juana Domínguez',
+        rate: 1000,
+        client:'zara',
+        location: 'Calle leganitos',
+        job: 'maquetadora',
+        code: 986539,        
+        ambassador:'luisa rodriguez',
+        sendChecked: true,
+        PRLChecked:true,
+        clientSendChecked:true,
+        dataChecked:true,
+        status:1
+    },
+    {
+        developer: 'María Domínguez',
+        rate: 1000,
+        client:'zara',
+        location: 'Calle leganitos',
+        job: 'maquetadora',
+        code: 2222,        
+        ambassador:'luisa rodriguez',
+        sendChecked: true,
+        PRLChecked:true,
+        clientSendChecked:true,
+        dataChecked:true,
+        status:2
+    },
+    {
+        developer: 'David Domínguez',
+        rate: 1000,
+        client:'zara',
+        location: 'Calle leganitos',
+        job: 'maquetadora',
+        code: 3333,        
+        ambassador:'luisa rodriguez',
+        sendChecked: true,
+        PRLChecked:true,
+        clientSendChecked:true,
+        dataChecked:true,
+        status:3
+    },
+    {
+        developer: 'Alejandra Domínguez',
+        rate: 1000,
+        client:'zara',
+        location: 'Calle leganitos',
+        job: 'maquetadora',
+        code: 44444,        
+        ambassador:'luisa rodriguez',
+        sendChecked: true,
+        PRLChecked:true,
+        clientSendChecked:true,
+        dataChecked:true,
+        status:4
+    },
+    {
+        developer: 'Pedro Domínguez',
+        rate: 1000,
+        client:'zara',
+        location: 'Calle leganitos',
+        job: 'maquetadora',
+        code: 55555,        
+        ambassador:'luisa rodriguez',
+        sendChecked: true,
+        PRLChecked:true,
+        clientSendChecked:true,
+        dataChecked:true,
+        status:5
+    },
+]
