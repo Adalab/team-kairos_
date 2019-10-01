d5b0454f89e844fdefdf9e3a4f97a035fd738ca5
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-10-01 13:13:47 +0200

    add display flex on newprocessbutton

--- src/components/NewProcess.js
+++ src/components/NewProcess.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Form1NewProject from './Form1NewProject';
+import '../styles/NewProcess.scss';
 
 const NewProcess = props => {
   return (
-    <main>
+    <main className="main__new-process">
       <form action="">
         <Form1NewProject />
-        <div className="button__container">
+        <div className="new-process__button-container">
           <Link to="/developerlist"><button className="create__button" type="submit">Iniciar Proceso</button></Link>
         </div>
       </form>
     </main>
-
   );
 }
 export default NewProcess;
