dd6cceaa3cb2a5e7c1d62458716e159074e1839c
claradios <clara.dios.diez@gmail.com>
2019-10-01 11:37:36 +0200

    ammbassador working

--- src/components/Form3Ambassador.js
+++ src/components/Form3Ambassador.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Form3Ambassador = props => {
+  const {rol} = props;
   return (
     <fieldset>
     <label htmlFor="sendChecked">Enviar mail a empleado</label>
-    <input type="checkbox" id="sendChecked" name="sendChecked" />
+    <input type="checkbox" id="sendChecked" name="sendChecked" disabled={!(rol==='ambassador')&&'disabled'} />
   </fieldset>
   );
 
